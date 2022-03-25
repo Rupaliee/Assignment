f942ba714982b625272407de0691ea6c7dd55639
akshada <saroderupss18@gmail.com>
2022-03-25 14:43:48 +0530

    number in word

---
+++ num_in_word.sh
@@ -0,0 +1,33 @@
+read -p " Enter a single digit number: " no
+if [ $no -eq 0 ]
+then
+    echo " zero "
+elif [ $no -eq 1 ]
+then
+    echo " one "
+elif [ $no -eq 2 ]
+then 
+     echo " two "
+elif [ $no -eq 3 ]
+then
+     echo " three "
+elif [ $no -eq 4 ]
+then
+     echo " four "
+elif [ $no -eq 5 ]
+then
+     echo " five "
+elif [ $no -eq 6 ]
+then
+     echo " Six "
+elif [ $no -eq 7 ]
+then
+     echo " seven "
+elif [ $no -eq 8 ]
+then
+     echo " eight "
+elif [ $no -eq 9 ]
+then
+     echo " nine "
+fi
+
